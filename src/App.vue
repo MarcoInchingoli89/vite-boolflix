b6e6d1fb13badbad88b0cf827d4aa0713eec9601
Marco Inchingoli <evilmarkus@hotmail.it>
2022-11-16 18:31:25 +0100

    Add Img Flags (Working Progress)

--- src/App.vue
+++ src/App.vue
@@ -6,14 +6,17 @@ export default {
   data() {
     return {
       store,
+      flagIta: "it",
+      flagEng: "en"
     }
   },
   methods: {
+    /* Cerco il film */
     searchMovies() {
       console.log('Hai cliccato')
       const searchInput = this.store.searchInput;
       console.log(searchInput);
-
+      /* Configurazione variabili axios */
       const config = {
         method: 'get',
         url: `${this.store.api_url}`,
@@ -23,7 +26,7 @@ export default {
           query: `${this.store.searchInput}`,
         }
       };
-
+      /* Chiamata Axios */
       axios(config)
         .then(function (response) {
           console.log(response);
@@ -44,10 +47,20 @@ export default {
 <template>
   <input @keyup.enter="searchMovies" v-model="store.searchInput" type="search" placeholder="Search a movie">
   <button @click="searchMovies">Search</button>
+  <!-- Stampo le caratteristiche dei film in pagina -->
   <ul v-for="movie in store.movies">
     <li>{{ movie.title }}</li>
     <li>{{ movie.original_title }}</li>
-    <li>{{ movie.original_language }}</li>
+    <!-- Inserisco le immagini delle bandiere -->
+    <li v-if="flagIta === movie.original_language">
+      <img width="50" :src="store.api_flag_it" alt="">
+    </li>
+    <li v-else-if="flagEng === movie.original_language">
+      <img width="50" :src="store.api_flag_us" alt="">
+    </li>
+    <li v-else>
+      {{ movie.original_language }}
+    </li>
     <li>{{ movie.vote_average }}</li>
   </ul>
 </template>
--- src/store.js
+++ src/store.js
@@ -5,7 +5,9 @@ import {
 //Creo le variabili del mio global state
 export const store = reactive({
     api_url: 'https://api.themoviedb.org/3/search/movie',
+    api_flag_it: 'https://countryflagsapi.com/svg/it',
+    api_flag_us: 'https://countryflagsapi.com/svg/us',
     movies: null,
     api_key: '7f8d78fcf72d33f11a9755a50a65bd33',
-    searchInput: ''
+    searchInput: '',
 })
