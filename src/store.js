a9e7054ad5cb60804e49d0c114d31326bdb69f55
Marco Inchingoli <evilmarkus@hotmail.it>
2022-11-16 12:26:08 +0100

    Add global state

---
+++ src/store.js
@@ -0,0 +1,11 @@
+import {
+    reactive
+} from 'vue'
+
+//Creo le variabili del mio global state
+export const store = reactive({
+    api_url: '',
+    movies: null,
+    api_key: '',
+    query: ''
+})
