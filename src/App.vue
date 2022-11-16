4e88e8135c8c3f71672af0ee359f2085e6516149
Marco Inchingoli <evilmarkus@hotmail.it>
2022-11-16 16:35:10 +0100

    Add params axios call

--- src/App.vue
+++ src/App.vue
@@ -13,18 +13,26 @@ export default {
       console.log('Hai cliccato')
       const searchInput = this.store.searchInput;
       console.log(searchInput);
+      /* const url = `${this.store.api_url}?api_key=${this.store.api_key}&language=en-US&query=${this.store.searchInput}&page=1&include_adult=false`
+      console.log(url) */
     }
   },
   /* Chiamata Axios */
   mounted() {
     var config = {
       method: 'get',
-      url: 'https://api.themoviedb.org/3/search/movie?api_key=7f8d78fcf72d33f11a9755a50a65bd33&language=en-US&query=Matrix&page=1&include_adult=false'
+      url: `${this.store.api_url}`,
+      params: {
+        api_key: `${this.store.api_key}`,
+        query: 'Matrix',
+      }
     };
+
+
 
     axios(config)
       .then(function (response) {
-
+        console.log(response)
         console.log(response.data);
       })
 
@@ -49,8 +57,8 @@ export default {
 
 <!-- TODO -->
 <!-- Chiamata Axios OK -->
-<!-- Funzione click su button -->
-<!-- V-Model sull'input -->
+<!-- Funzione click su button OK -->
+<!-- V-Model sull'input OK-->
 <!-- Crea un ul per stampare i film OK -->
 <!-- Ciclo v-for per il singolo film -->
 <!-- Collegare l'api con la ricerca dei film alla variabile searchInput del file store -->
--- src/store.js
+++ src/store.js
@@ -4,9 +4,8 @@ import {
 
 //Creo le variabili del mio global state
 export const store = reactive({
-    api_url: '',
+    api_url: 'https://api.themoviedb.org/3/search/movie',
     movies: null,
-    api_key: '',
-    query: '',
+    api_key: '7f8d78fcf72d33f11a9755a50a65bd33',
     searchInput: ''
 })
